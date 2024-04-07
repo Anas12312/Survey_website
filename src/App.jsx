8571e7cad952de710ef21260efffe7181f82d24a
anas12312 <anashesham000@gmail.com>
2024-04-07 22:15:25 +0200

    anas

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import Questions from './Components/Questions'
 
 function App() {
 
   return (
-    <div>
-      Empty
+    <div className='w-screen h-screen '>
+      <Questions />
     </div>
   )
 }
