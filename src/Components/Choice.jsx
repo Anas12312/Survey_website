0aea86930e3a682aac416134c762073b3d52e4fe
anas12312 <anashesham000@gmail.com>
2024-04-07 23:00:50 +0200

    anas

--- src/Components/Choice.jsx
+++ src/Components/Choice.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useRef } from 'react'
 
-export default function Choice({text, onSubmit}) {
-  return (
-    <div>
-        {text}
-    </div>
-  )
+export default function Choice({ text, onSubmit }) {
+    const ref = useRef(null);
+
+    const refade = () => {
+
+        ref.current.classList.toggle('fade-in-image');
+        setTimeout(() => {
+            ref.current.classList.toggle('fade-in-image');
+        }, 100)
+
+        setTimeout(() => {
+            ref.current.classList.toggle('fade-in-image');
+        }, 200)
+
+        setTimeout(() => {
+            ref.current.classList.toggle('fade-in-image');
+        }, 300)
+        setTimeout(() => {
+            onSubmit()
+        }, 400)
+
+    }
+    return (
+        <div onClick={refade} ref={ref} className='w-[50%] h-10 flex justify-center items-center bg-[#e5ecf7] fade-in-image border text-[#0445af] 
+                                border-[#0445af] rounded-md 
+                                my-3 cursor-pointer'>
+            {text}
+        </div>
+    )
 }
--- src/Components/Questions.jsx
+++ src/Components/Questions.jsx
@@ -1,40 +1,108 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Question from './Question'
 
 export default function Questions() {
     const [questionNumber, setQuestionNumber] = useState(1)
     const nextQuestion = () => {
-        setQuestionNumber(nextQuestion + 1)
+        setQuestionNumber(questionNumber + 1)
     }
-  return (
-    <div className='h-full w-full'>
-        {questionNumber==1?(
-            <div className='flex h-full'>
-                <Question
-                    question={"Are you a victoria's secret fan?"}
-                    description={null}
-                    choices={
-                        [
-                            "Yes",
-                            "No"
-                        ]
-                    }
-                    questionNumber={questionNumber}
-                    nextQuestion={nextQuestion}
-                />
-                <div className='h-full w-full border border-green-500'>
-
+    return (
+        <div className='h-full w-full'>
+            {questionNumber == 1 ? (
+                <div className='flex h-full w-full'>
+                    <Question
+                        question={"Are you a victoria's secret fan?"}
+                        description={null}
+                        choices={
+                            [
+                                {
+                                    text: "Yes",
+                                    selected: false
+                                },
+                                {
+                                    text: "No",
+                                    selected: false
+                                }
+                            ]
+                        }
+                        questionNumber={1}
+                        nextQuestion={nextQuestion}
+                    />
+                    <div className='h-full w-[50%] overflow-hidden'>
+                        <img className='object-fit w-full' src="../../public/image1.avif" alt="" />
+                    </div>
                 </div>
-            </div>
-        ):(
-            questionNumber==2?(
-                <div></div>
-            ):questionNumber==3?(
-                <div></div>
-            ):(
-                <div></div>
-            )
-        )}
-    </div>
-  )
+            ) : (
+                questionNumber == 2 ? (
+                    <div className='flex h-full w-full'>
+                        <Question
+                            question={"Do you currently own any Victoria's Secret products?"}
+                            description={null}
+                            choices={
+                                [
+                                    {
+                                        text: "Yes",
+                                        selected: false
+                                    },
+                                    {
+                                        text: "No",
+                                        selected: false
+                                    }
+                                ]
+                            }
+                            questionNumber={2}
+                            nextQuestion={nextQuestion}
+                        />
+                        <div className='h-full w-[50%] overflow-hidden'>
+                        <img className='object-fit w-full' src="../../public/image2.avif" alt="" />
+                    </div>
+                    </div>
+                ) : questionNumber == 3 ? (
+                    <div className='flex h-full w-full'>
+                        <Question
+                            question={"Have you ever won in a survey?"}
+                            description={null}
+                            choices={
+                                [
+                                    {
+                                        text: "Yes",
+                                        selected: false
+                                    },
+                                    {
+                                        text: "No",
+                                        selected: false
+                                    }
+                                ]
+                            }
+                            questionNumber={3}
+                            nextQuestion={nextQuestion}
+                        />
+                        <div className='h-full w-[50%] overflow-hidden'>
+                        <img className='object-fit w-full' src="../../public/image3.avif" alt="" />
+                    </div>
+                    </div>
+                ) : (
+                    <div className='flex h-full w-full'>
+                        <Question
+                            question={"Ready to win a gift card to Victoria's Secret?"}
+                            description={null}
+                            choices={
+                                [
+                                    {
+                                        text: "Yes",
+                                        selected: false
+                                    }
+                                ]
+                            }
+                            questionNumber={4}
+                            nextQuestion={nextQuestion}
+                        />
+                        <div className='h-full w-[50%] overflow-hidden'>
+                        <img className='object-fit w-full' src="../../public/image4.avif" alt="" />
+                    </div>
+                    </div>
+                )
+            )}
+        </div>
+    )
 }
