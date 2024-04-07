c0d8964947083a3354dd40a8bb78b993b9f22a5b
Zyad Abdul-Nasser <zizo_007@live.com>
2024-04-07 22:27:45 +0200

    3lyaltaa2_kollo

--- src/Components/Choice.jsx
+++ src/Components/Choice.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 
-export default function Choice({text, onSubmit}) {
+export default function Choice({ text, onSubmit }) {
   return (
-    <div>
+    <button onClick={onSubmit} className="p-[3px] relative">
+      <div className="absolute inset-0 bg-gradient-to-r from-indigo-500 to-purple-500 rounded-lg" />
+      <div className="px-8 py-2  bg-black rounded-[6px]  relative group transition duration-200 text-white hover:bg-transparent">
         {text}
-    </div>
+      </div>
+    </button>
   )
 }
